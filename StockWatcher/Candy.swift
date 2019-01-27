cfaca2b5eb21c32766f43349e1d76c7bdda4ae35
Loey Agdan <loeyagdan0225@gmail.com>
2019-01-27 17:39:31 +0400

    add todo code cleanup

---
+++ StockWatcher/Candy.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+struct Candy {
+    let category : String
+    let name : String
+    let volume: String
+    let percent_change: String
+    let price: String
+    let currency: String
+}
