761194f9adbb4b98b8ad8dbd187c0ba38532e55c
Loey Agdan <loeyagdan0225@gmail.com>
2019-01-31 15:37:05 +0400

    update placeholder in + portfolio

--- StockWatcher/MonitorViewController.swift
+++ StockWatcher/MonitorViewController.swift
@@ -8,12 +8,13 @@
 
 /*
     - Keyboard number
-    - Save via CoreData
+    X Save via CoreData
     - Automatic computation on textfield change
  
  */
 
 import UIKit
+import CoreData
 
 class MonitorViewController: UIViewController {
 
@@ -26,7 +27,12 @@ class MonitorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.setToolbarHidden(false, animated: false)
+        navigationController?.setToolbarHidden(true, animated: false)
+        price.placeholder = "0.00"
+        txtStocks.placeholder = "0"
+        txtAmount.placeholder = "0.00"
+        txtCharge.placeholder = "0.00"
+        txtTax.placeholder = "0.00"
     }
     
     @IBAction func btnSaveTapped(_ sender: UIButton) {
@@ -38,7 +44,44 @@ class MonitorViewController: UIViewController {
         let stocks = Double(txtStocks.text!)
         let c = Double(txtCharge.text!)!
         let tax = Double(txtTax.text!)!
-        let amountTotal = (stockPrice! * stocks!) - ( c + tax )
+        var amountTotal = (stockPrice! * stocks!) - ( c + tax )
+        txtAmount.text = String(stockPrice! * stocks!)
         lblTotalAmtInvested.text = String(amountTotal)
+        
+//        if amountTotal > 0 {
+//            //TODO:- Add confirmation to save here...
+//            saveInv()
+//        }
+        
+    }
+    
+    func saveInv(){
+//        if Double(lblTotalAmtInvested.text!)! > 0 {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            
+            let managedContext = appDelegate.persistentContainer.viewContext
+            let entity = NSEntityDescription.entity(forEntityName: "Investment", in: managedContext)!
+            let portfolio = NSManagedObject(entity: entity, insertInto: managedContext)
+            
+            portfolio.setValue(Double(txtCharge.text!)!, forKey: "bcharge")
+            portfolio.setValue(Double(price.text!)!, forKey: "price")
+            portfolio.setValue(Int(txtStocks.text!)!, forKey: "stocks")
+            portfolio.setValue(Double(txtTax.text!)!, forKey: "tax")
+            portfolio.setValue(Double(txtAmount.text!)!, forKey: "amount")
+            portfolio.setValue(Double(lblTotalAmtInvested.text!)!, forKey: "total")
+            portfolio.setValue(UUID.init().uuidString, forKey: "id")
+            portfolio.setValue("\(Date())", forKey: "date")
+            portfolio.setValue(String(""), forKey: "code")
+            
+            do{
+                try managedContext.save()
+                print("Investment saved...")
+            }catch let error as NSError {
+                print("\(error)")
+            }
+            
+//        }
     }
 }
