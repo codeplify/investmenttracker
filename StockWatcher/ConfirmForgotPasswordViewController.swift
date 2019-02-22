15a5b024db7565d14e440bd36998b7186f5b7ab4
loeya <loeyagdan0225@gmail.com>
2019-02-22 17:49:44 +0400

    + forgot password viewcontroller + confirm password viewcontroller

---
+++ StockWatcher/ConfirmForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConfirmForgotPasswordViewController.swift
+//  StockWatcher
+//
+//  Created by Loey Agdan on 2/22/19.
+//  Copyright © 2019 Loey Agdan. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
