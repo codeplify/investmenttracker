b62efc6cc2e33184d1cd83497426a32a20f065d5
loeya <loeyagdan0225@gmail.com>
2019-02-22 18:22:59 +0400

    -! forgot password logic and confirmation

--- StockWatcher/ConfirmForgotPasswordViewController.swift
+++ StockWatcher/ConfirmForgotPasswordViewController.swift
@@ -7,24 +7,53 @@
 //
 
 import UIKit
+import AWSCognitoIdentityProvider
 
 class ConfirmForgotPasswordViewController: UIViewController {
 
+    var pool: AWSCognitoIdentityUserPool?
+    var user: AWSCognitoIdentityUser?
+    
+    @IBOutlet weak var txtCode: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnChangedPasswordPressed(_ sender: Any) {
+        guard let confirmationCodeValue = self.txtCode.text, !confirmationCodeValue.isEmpty else {
+            let alertController = UIAlertController(title: "Password Field Empty",
+                                                    message: "Please enter a password of your choice.",
+                                                    preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion:  nil)
+            return
+        }
+        
+        //confirm forgot password with input from ui.
+        self.user?.confirmForgotPassword(confirmationCodeValue, password: self.txtPassword.text!).continueWith {[weak self] (task: AWSTask) -> AnyObject? in
+            guard let strongSelf = self else { return nil }
+            DispatchQueue.main.async(execute: {
+                if let error = task.error as NSError? {
+                    let alertController = UIAlertController(title: error.userInfo["__type"] as? String,
+                                                            message: error.userInfo["message"] as? String,
+                                                            preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(okAction)
+                    
+                    self?.present(alertController, animated: true, completion:  nil)
+                } else {
+                    let _ = strongSelf.navigationController?.popToRootViewController(animated: true)
+                }
+            })
+            return nil
+        }
     }
-    */
+    
 
 }
--- StockWatcher/ForgotPasswordViewController.swift
+++ StockWatcher/ForgotPasswordViewController.swift
@@ -7,24 +7,60 @@
 //
 
 import UIKit
+import AWSCognitoIdentityProvider
 
 class ForgotPasswordViewController: UIViewController {
 
+    var pool: AWSCognitoIdentityUserPool?
+    var user: AWSCognitoIdentityUser?
+    
+    @IBOutlet weak var txtEmailAddress: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.pool = AWSCognitoIdentityUserPool(forKey: AWSCognitoSigninProviderKey)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let newPasswordViewController = segue.destination as? ConfirmForgotPasswordViewController {
+            newPasswordViewController.user = self.user
+        }
     }
-    */
+    
+    @IBAction func btnForgotPassword(_ sender: Any) {
+        guard let username = self.txtEmailAddress.text, !username.isEmpty else {
+            
+            let alertController = UIAlertController(title: "Missing UserName",
+                                                    message: "Please enter a valid user name.",
+                                                    preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion:  nil)
+            return
+        }
+        
+        self.user = self.pool?.getUser(self.txtEmailAddress.text!)
+        self.user?.forgotPassword().continueWith{[weak self] (task: AWSTask) -> AnyObject? in
+            guard let strongSelf = self else {return nil}
+            DispatchQueue.main.async(execute: {
+                if let error = task.error as NSError? {
+                    let alertController = UIAlertController(title: error.userInfo["__type"] as? String,
+                                                            message: error.userInfo["message"] as? String,
+                                                            preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                    alertController.addAction(okAction)
+                    
+                    self?.present(alertController, animated: true, completion:  nil)
+                } else {
+                    strongSelf.performSegue(withIdentifier: "confirmForgotPasswordSegue", sender: sender)
+                }
+            })
+            return nil
+        }
+    }
+    
+    
 
 }
