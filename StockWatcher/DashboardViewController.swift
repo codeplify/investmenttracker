723cdda9eaea2bfbacc6712ae6c8794b76f0ee6c
Loey Agdan <loeyagdan0225@gmail.com>
2019-02-27 11:10:08 +0400

    + push all reachability class

--- StockWatcher/DashboardViewController.swift
+++ StockWatcher/DashboardViewController.swift
@@ -10,7 +10,6 @@
     TODO:- Add loading current status of invested stocks
     TOFIX:- Back button on detailview fix splitview issue
     TODO:- Add AdMob
-    TODO:- Add Internet connectivity
  */
 
 import UIKit
@@ -30,6 +29,8 @@ class DashboardViewController: UIViewController, UITableViewDelegate , UITableVi
     var response: AWSCognitoIdentityUserGetDetailsResponse?
     var ps:[Portfolio] = [Portfolio]()
     
+    let network: NetworkManager = NetworkManager.sharedInstance
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -47,6 +48,34 @@ class DashboardViewController: UIViewController, UITableViewDelegate , UITableVi
         
         self.searchP()
         self.refresh()
+        
+        checkNetworkConnectivity()
+    }
+    
+    func checkNetworkConnectivity(){
+        NetworkManager.isUnreachable { _ in
+            print("Network manager is unreachable...")
+            let alert = UIAlertController(title: "Unable to connect", message: "Please check your internet connectivity", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+        NetworkManager.isReachable { _ in
+            print("Network is reachable")
+           
+        }
+        
+        network.reachability.whenReachable = { _ in
+            print("Network connectivity success")
+           
+        }
+        
+        network.reachability.whenUnreachable = {
+            _ in
+            print("Network connectivity failed")
+           
+        }
     }
     
     
